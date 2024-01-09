9349898043d855c1a4d6fb76c380b25308c0b6d7
WaneeSilaprapat <waneesilprapat@gmail.com>
2024-01-09 21:56:05 +0700

    modified main and requirements

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st 
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+import matplotlib as plt
 
 st.set_page_config(layout="wide")
 st.title("Retail Analytics")
