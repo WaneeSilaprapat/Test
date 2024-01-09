d0dab7f8519c8361dc019f0ea657c841cbc16929
jirametoon <sendtocartoon@gmail.com>
2024-01-10 00:35:12 +0700

    sungsung2

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st 
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+from matplotlib import pyplot as plt
 
 st.set_page_config(layout="wide")
 st.title("Retail Analytics")
